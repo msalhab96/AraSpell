7236e15d1b2839cabde865e01726d6f8d7d40de8
msalhab96 <mahmoud@salhab.work>
2022-07-30 00:04:56 +0300

    processes.py added

---
+++ processes.py
@@ -0,0 +1,91 @@
+from typing import Union
+import re
+from typing import List
+from interfaces import IProcess
+from utils import load_text_file, remove_long_spaces
+
+
+class LoadFile(IProcess):
+
+    def execute(self, file_path: str):
+        return load_text_file(
+            file_path
+            )
+
+
+class LinesSplitter(IProcess):
+    def __init__(self, sep: str) -> None:
+        super().__init__()
+        self.sep = sep
+
+    def split(self, line):
+        return line.split(self.sep)
+
+    def execute(self, data: Union[List[str], str]) -> List[str]:
+        if isinstance(data, str):
+            return data.split(self.sep)
+        results = []
+        for lines in map(self.split, data):
+            results.extend(lines)
+        return results
+
+
+class LengthFilter(IProcess):
+    def __init__(
+            self, min_length: int, max_length: int
+            ) -> None:
+        super().__init__()
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def execute(self, lines: List[str]):
+        return list(filter(
+            lambda x: self.min_length <= len(x) <= self.max_length, lines
+            ))
+
+
+class CharsRemover(IProcess):
+    def __init__(self, chars: str) -> None:
+        super().__init__()
+        self.pat = f'[{chars}]'
+
+    def remove(self, line: str) -> str:
+        return re.sub(self.pat, '', line)
+
+    def execute(self, lines: List[str]) -> List[str]:
+        return map(self.remove, lines)
+
+
+class RepeatedCharsCollapsor(IProcess):
+    def __init__(self, max_repeteion: int) -> None:
+        super().__init__()
+        self.pat = r"(.)\1{}".format(f"{{{2},}}")
+
+    def collaps(self, line: str) -> str:
+        return re.sub(self.pat, r"\1" * 1, line)
+
+    def execute(self, lines: List[str]) -> List[str]:
+        return list(map(self.collaps, lines))
+
+
+class ValidCharsKeeper(IProcess):
+    def __init__(self, valid_chars: str, rep_with=' ') -> None:
+        super().__init__()
+        self.valid_chars = valid_chars
+        self.rep_with = rep_with
+        self.pat = f'[^{self.valid_chars}]'
+
+    def __keep(self, line: str) -> str:
+        return re.sub(self.pat, ' ', line)
+
+    def execute(self, lines: List[str]) -> List[str]:
+        return list(map(self.__keep, lines))
+
+
+class SpacesRemover(IProcess):
+
+    def __remove(self, line: str) -> str:
+        return remove_long_spaces(line).strip()
+
+    def execute(self, lines: List[str]):
+        return list(map(self.__remove, lines))
