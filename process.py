bf0fb98a3b97a89e156353278832351e4bc318a3
msalhab96 <mahmoud@salhab.work>
2022-07-30 19:21:35 +0300

    TextDistorter added

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from random import random
 from typing import Union, Any, List
 from interfaces import IProcess, IProcessor
 import concurrent.futures
@@ -6,7 +7,7 @@ import concurrent.futures
 
 class FilesProcessor(IProcessor):
     def __init__(
-            self, processes: IProcess
+            self, processes: List[IProcess]
             ) -> None:
         self.processes = processes
 
@@ -30,3 +31,23 @@ class FilesProcessor(IProcessor):
         with concurrent.futures.ThreadPoolExecutor() as executor:
             result = executor.map(self.file_run, files)
         return list(result)
+
+
+class TextDistorter(IProcessor):
+    def __init__(
+            self, ratio: float, processes: List[IProcess]
+            ) -> None:
+        super().__init__()
+        self.ratio = ratio
+        self.processes = processes
+
+    def run(self, line: str) -> str:
+        length = len(line)
+        n = int(self.ratio * length)
+        for _ in range(n):
+            line = random.choice(self.processes)(line)
+        return line
+
+    def dist_run(self):
+        # TODO
+        pass
