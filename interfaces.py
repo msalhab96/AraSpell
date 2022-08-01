41f342c309274764b1115e74cbd1deef1822b9c3
msalhab96 <mahmoud@salhab.work>
2022-08-01 19:42:13 +0300

    Itokenizer added

--- interfaces.py
+++ interfaces.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 
 
 class IProcess(ABC):
@@ -16,4 +16,47 @@ class IProcessor(ABC):
 
     @abstractmethod
     def dist_run():
+        pass
+
+
+class ITokenizer(ABC):
+
+    @abstractmethod
+    def ids2tokens(self):
+        pass
+
+    @abstractmethod
+    def tokenize(self):
+        pass
+
+    @abstractmethod
+    def set_tokenizer(self):
+        pass
+
+    @abstractmethod
+    def save_tokenizer(self):
+        pass
+
+    @abstractmethod
+    def load_tokenizer(self):
+        pass
+
+    @abstractmethod
+    def add_token(self):
+        pass
+
+    @abstractmethod
+    def preprocess_tokens(self):
+        pass
+
+    @abstractmethod
+    def batch_tokenizer(self):
+        pass
+
+    @abstractproperty
+    def vocab_size(self):
+        pass
+
+    @abstractmethod
+    def get_tokens(self):
         pass
