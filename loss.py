46c85b9271049dfdad67cf5f4d89fd5f409088f5
msalhab96 <mahmoud@salhab.work>
2022-08-03 18:27:13 +0300

    Loss updated to include kl and lable smoothing

--- loss.py
+++ loss.py
@@ -1,18 +1,68 @@
 import torch.nn as nn
 from torch import Tensor
+from torch.nn.functional import one_hot
 
 
 class Loss(nn.Module):
-    def __init__(self, pad_idx: int) -> None:
+    def __init__(
+            self,
+            voc_size: int,
+            alpha=0.1
+            ) -> None:
         super().__init__()
-        self.cel = nn.CrossEntropyLoss(ignore_index=pad_idx)
+        self.kld = nn.KLDivLoss(reduction='batchmean')
+        self.voc_size = voc_size
+        self.alpha = alpha
 
-    def forward(self, results: Tensor, targets: Tensor) -> Tensor:
+    def process_target(self, target: Tensor, mask: Tensor) -> Tensor:
+        """Processes the target tensor of shape [B, M]
+        by converting the sparse target into uniform distribution
+        using lable smoothing.
+
+        Args:
+            target (Tensor): The target Tensor of shape [B, M]
+            mask (Tensor): The target mask tensor of shape [B, M]
+            where it's True whereever there's a padding.
+
+        Returns:
+            Tensor: The processed Tensor of shape [B, M - 1, V]
+        """
+        target = target[:, 1:]
+        target = target.reshape(-1)
+        target = one_hot(
+            target, num_classes=self.voc_size
+            ).to(target.device)
+        weight = self.alpha / self.voc_size
+        target = target * (1 - self.alpha) + weight
+        target = (~mask.view(-1, 1)) * target
+        return target
+
+    def procees_preds(self, preds: Tensor, mask: Tensor) -> Tensor:
+        """Processes the model's results tensor of shape [B, M, V]
+        by conducting masking and tensor reshaping to the original tensor
+
+        Args:
+            preds (Tensor): The model's results of shape [B, M, V].
+            mask (Tensor): The masking tensor of shape [B, M-1, V].
+
+        Returns:
+            Tensor: The processed tensor of shape [B, M-1, V]
+        """
+        preds = preds[:, :-1, :]
+        preds = preds.reshape(-1, preds.shape[-1])
+        preds = (~preds.reshape(-1, 1)) * preds
+        return preds
+
+    def forward(
+            self,
+            preds: Tensor,
+            target: Tensor,
+            mask: Tensor
+            ) -> Tensor:
         # results of shape [B, M, voc_size]
         # targets of shape [B, M]
-        results = results[:, :-1, :]
-        targets = targets[:, 1:]
-        v = results.shape[-1]
-        targets = targets.reshape(-1)
-        results = results.reshape(-1, v)
-        return self.cel(results, targets)
+        mask = mask[:, 1:]
+        mask = mask.to(preds.device)
+        target = self.process_target(target, mask)
+        preds = self.procees_preds(preds, mask)
+        return self.kld(preds, target)
