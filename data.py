23d6fc3a906481845fc4222b083ebe26f7fbf0a1
msalhab96 <mahmoud@salhab.work>
2022-08-03 18:07:28 +0300

    data.py refactored to load the data from csv

--- data.py
+++ data.py
@@ -1,35 +1,34 @@
 from pathlib import Path
 from typing import Tuple, Union
-from interfaces import IProcessor, ITokenizer
+from interfaces import ITokenizer
 from torch.utils.data import Dataset, DataLoader
-from utils import load_text_file
 import torch
 from torch.utils.data.distributed import DistributedSampler
+import pandas as pd
+from torch import Tensor
 
 
 class ArabicData(Dataset):
     def __init__(
             self,
             data_path: Union[str, Path],
-            distortion_processor: IProcessor,
             tokenizer: ITokenizer,
-            batch_size: int,
             pad_idx: int,
-            hold=True
+            max_len: int,
+            ratio: float,
+            dist_key='distorted',
+            clean_key='clean'
             ) -> None:
         super().__init__()
+        self.data_path = data_path
         self.tokenizer = tokenizer
-        self.data = load_text_file(data_path).split('\n')
-        self.lengths = list(map(len, self.data))
-        self.dist_proc = distortion_processor
-        ratio = distortion_processor.ratio
-        self.max_lengths = list(map(
-            lambda l: l + int(ratio * l),
-            self.lengths
-            ))
-        self.batch_size = batch_size
-        self._cache = []
+        self.max_len = max_len + 2
+        self.dist_key = dist_key
+        self.clean_key = clean_key
         self.pad_idx = pad_idx
+        self.max_dist_len = int(ratio * max_len) + max_len
+        self.__data = []
+        self.df = pd.read_csv(data_path)
 
     def pad(self, line: list, max_len: int) -> Tuple[list, int]:
         length = len(line)
@@ -37,45 +36,52 @@ class ArabicData(Dataset):
         assert diff >= 0
         return line + [self.pad_idx] * diff, diff
 
-    def __getitem__(self, idx: int):
-        # TODO: Refactor this part of code
-        if idx < len(self._cache):
-            return self._cache[idx]
-        item = self.data[idx]
-        dist_item = self.dist_proc.run(item)
-        item = self.tokenizer.tokenize(item, add_sos=True, add_eos=True)
-        max_len = 800
-        item, diff = self.pad(item, max_len)
-        item_mask = [False] * (max_len - diff) + [True] * diff
-        max_len = 900
-        dist_item = self.tokenizer.tokenize(dist_item)
-        dist_item, diff = self.pad(dist_item, 900)
-        dist_item_mask = [False] * (max_len - diff) + [True] * diff
+    def _get_clean(self, idx: int) -> Tuple[Tensor, Tensor]:
+        item = self.df.iloc[idx][self.clean_key]
+        item = self.tokenizer.tokenize(
+            item, add_sos=True, add_eos=True
+            )
+        mask = [False] * len(item)
+        item, diff = self.pad(item, self.max_len)
+        mask += [True] * diff
         item = torch.LongTensor(item)
-        dist_item = torch.LongTensor(dist_item)
-        item_mask = torch.BoolTensor(item_mask)
-        dist_item_mask = torch.BoolTensor(dist_item_mask)
-        self._cache.append((dist_item, item, dist_item_mask, item_mask))
-        return dist_item, item, dist_item_mask, item_mask
+        mask = torch.BoolTensor(mask)
+        return item, mask
+
+    def _get_distorted(self, idx: int) -> Tuple[Tensor, Tensor]:
+        item = self.df.iloc[idx][self.dist_key]
+        item = self.tokenizer.tokenize(item)
+        mask = [False] * len(item)
+        item, diff = self.pad(item, self.max_dist_len)
+        mask += [True] * diff
+        item = torch.LongTensor(item)
+        mask = torch.BoolTensor(mask)
+        return item, mask
+
+    def __getitem__(self, idx: int):
+        clean, clean_mask = self._get_clean(idx)
+        distorted, distorted_mask = self._get_distorted(idx)
+        return distorted, clean, distorted_mask, clean_mask
 
     def __len__(self):
-        return len(self.data)
+        return self.df.shape[0]
 
 
 def get_dist_data_laoder(
         data_path,
-        distortion_processor,
         tokenizer,
+        max_len,
+        ratio,
         batch_size,
         rank,
         world_size
         ):
     dataset = ArabicData(
         data_path=data_path,
-        distortion_processor=distortion_processor,
         tokenizer=tokenizer,
-        batch_size=batch_size,
-        pad_idx=tokenizer.special_tokens.pad_id
+        pad_idx=tokenizer.special_tokens.pad_id,
+        max_len=max_len,
+        ratio=ratio
     )
     sampler = DistributedSampler(
         dataset=dataset,
@@ -93,16 +99,17 @@ def get_dist_data_laoder(
 
 def get_data_laoder(
         data_path,
-        distortion_processor,
         tokenizer,
-        batch_size
+        batch_size,
+        max_len,
+        ratio,
         ):
     dataset = ArabicData(
         data_path=data_path,
-        distortion_processor=distortion_processor,
         tokenizer=tokenizer,
-        batch_size=batch_size,
-        pad_idx=tokenizer.special_tokens.pad_id
+        pad_idx=tokenizer.special_tokens.pad_id,
+        max_len=max_len,
+        ratio=ratio
     )
     return DataLoader(
         dataset,
