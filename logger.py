1103ca2e22951fcc43dad3d2a2978236a05c1cf9
msalhab96 <mahmoud@salhab.work>
2022-08-05 14:47:32 +0300

    log_img added to log the attention

--- logger.py
+++ logger.py
@@ -3,14 +3,36 @@ from pathlib import Path
 from typing import Any, Union
 from interfaces import ILogger
 from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+from torch import Tensor
 
 
 class BasicLogger(ILogger):
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(
+            self,
+            log_dir: Union[str, Path],
+            *args, **kwargs
+            ) -> None:
         super().__init__()
+        self.log_dir = log_dir
         self.__rank = 0
         self.__tag_format = '{}_{}'
+        self.steps = {}
+
+    def get_step(self, tag: str) -> int:
+        step = self.steps.get(tag, 0)
+        if step == 0:
+            self.steps[tag] = 0
+            self.steps[tag] += 1
+            return step
+        self.steps[tag] += 1
+        return self.steps[tag]
+
+    def _get_img_path(self, tag: str) -> Union[Path, str]:
+        step = self.get_step(tag)
+        path = f'{tag}_{step}.png'
+        return os.path.join(self.log_dir, path), step
 
     def _get_tag(self, key: str) -> str:
         return self.__tag_format.format(key, self.__rank)
@@ -33,7 +55,35 @@ class BasicLogger(ILogger):
         if step is None:
             print(f'{tag}' + f': {value} ', end=end)
             return
-        print(f'{step}== ' + tag + f': {value} ', end=end)
+        print(f'{step} ==== ' + tag + f': {value} ', end=end)
+
+    def log_img(self, tag: str, images: Tensor) -> None:
+        n_imgs = images.shape[0]
+        if n_imgs == 1:
+            x = 1
+            y = 1
+        else:
+            x = n_imgs // 2
+            x += 0 if n_imgs % 2 == 0 else 1
+            y = 2
+        figure = plt.figure(figsize=(8, 8))
+        for i in range(1, 1 + n_imgs):
+            image = images[i - 1].squeeze().cpu().numpy()
+            ax = figure.add_subplot(x, y, i)
+            ax.pcolormesh(image)
+            ax.xaxis.label.set_size(4)
+            ax.yaxis.label.set_size(4)
+        figure.subplots_adjust(
+            left=0.1,
+            bottom=0.1,
+            right=0.9,
+            top=0.9,
+            wspace=0.4,
+            hspace=0.4
+            )
+        path, step = self._get_img_path(tag)
+        figure.savefig(path)
+        return path, step
 
 
 class TBLogger(BasicLogger):
@@ -43,18 +93,8 @@ class TBLogger(BasicLogger):
             log_dir: Union[str, Path],
             *args, **kwargs
             ) -> None:
-        super().__init__()
+        super().__init__(log_dir)
         self.writer = SummaryWriter(log_dir)
-        self.steps = {}
-
-    def get_step(self, tag: str) -> int:
-        step = self.steps.get(tag, 0)
-        if step == 0:
-            self.steps[tag] = 0
-            self.steps[tag] += 1
-            return step
-        self.steps[tag] += 1
-        return self.steps[tag]
 
     def log_step(self, key: str, value: Any):
         tag = self._get_tag(key)
@@ -72,6 +112,12 @@ class TBLogger(BasicLogger):
             key=key, value=value, step=step, end=end
             )
         self.writer.add_scalar(tag, value, self.get_step(tag))
+
+    def log_img(self, tag: str, images: Tensor) -> None:
+        path, step = super().log_img(tag, images)
+        img = plt.imread(path)[..., :3]
+        plt.close()
+        self.writer.add_image(tag, img, step, dataformats='HWC')
 
 
 def get_logger(args):
