72862f7898134143de574774c2bea8e6c3f71473
msalhab96 <mahmoud@salhab.work>
2022-08-23 21:06:54 +0300

    RNN model added

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
+from typing import Tuple
 from args import get_model_args
 from torch import nn
-from layers import DecoderLayers, EncoderLayers
+from layers import DecoderLayers, EncoderLayers, RNNDecoder, RNNEncoder
 from torch import Tensor
 
 
-class Model(nn.Module):
+class Transformer(nn.Module):
     def __init__(
             self,
             enc_params: dict,
@@ -61,7 +62,74 @@ class Model(nn.Module):
         return enc_inp, nn.functional.log_softmax(out, dim=-1), att
 
 
-def get_model(args, rank: int, voc_size: int, pad_idx: int) -> nn.Module:
-    return Model(
+class RNN(nn.Module):
+    def __init__(
+            self,
+            voc_size: int,
+            emb_size: int,
+            n_layers: int,
+            hidden_size: int,
+            bidirectional: bool,
+            padding_idx: int,
+            padding_value: int,
+            p_dropout: float,
+            max_len: int
+            ) -> None:
+        super().__init__()
+        self.encoder = RNNEncoder(
+            voc_size=voc_size,
+            emb_size=emb_size,
+            n_layers=n_layers,
+            hidden_size=hidden_size,
+            p_dropout=p_dropout,
+            bidirectional=bidirectional,
+            padding_idx=padding_idx,
+            padding_value=padding_value
+        )
+        self.decoder = RNNDecoder(
+            max_len=max_len,
+            voc_size=voc_size,
+            emb_size=emb_size,
+            n_layers=n_layers,
+            hidden_size=hidden_size,
+            p_dropout=p_dropout,
+            bidirectional=bidirectional,
+            padding_idx=padding_idx,
+            padding_value=padding_value
+        )
+
+    def get_lengths(self, mask: Tensor) -> Tensor:
+        return (~mask).sum(dim=-1)
+
+    def forward(
+            self,
+            enc_inp: Tensor,
+            dec_inp: Tensor,
+            enc_mask: Tensor,
+            dec_mask: Tensor
+            ) -> Tuple[Tensor, Tensor]:
+        enc_lengths = self.get_lengths(enc_mask)
+        dec_lengths = self.get_lengths(dec_mask)
+        enc_values, h = self.encoder(enc_inp, enc_lengths)
+        result, attention = self.decoder(
+            enc_values=enc_values,
+            hn=h,
+            x=dec_inp,
+            lengths=dec_lengths
+            )
+        return result, attention
+
+
+def get_model(
+        args,
+        rank: int,
+        voc_size: int,
+        pad_idx: int
+        ) -> nn.Module:
+    if args.model == 'rnn':
+        return RNN(
+            **get_model_args(args, voc_size, rank, pad_idx)
+        )
+    return Transformer(
         **get_model_args(args, voc_size, rank, pad_idx)
-    )
+        )
