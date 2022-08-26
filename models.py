94c36f2564778cb405ef2c66c99e0c4cf07e174f
msalhab96 <mahmoud@salhab.work>
2022-08-26 11:06:49 +0300

    Seq2SeqBasic added, predict added to Seq2SeqRNN

--- models.py
+++ models.py
@@ -1,8 +1,16 @@
-from typing import Tuple
+from typing import Tuple, Union
 from args import get_model_args
 from torch import nn
-from layers import DecoderLayers, EncoderLayers, RNNDecoder, RNNEncoder
+from layers import (
+    Attention,
+    DecoderLayers,
+    EncoderLayers,
+    PackedGRU,
+    RNNDecoder,
+    RNNEncoder
+    )
 from torch import Tensor
+import torch
 
 
 class Transformer(nn.Module):
@@ -62,7 +70,7 @@ class Transformer(nn.Module):
         return enc_inp, nn.functional.log_softmax(out, dim=-1), att
 
 
-class RNN(nn.Module):
+class Seq2SeqRNN(nn.Module):
     def __init__(
             self,
             voc_size: int,
@@ -119,6 +127,135 @@ class RNN(nn.Module):
             )
         return nn.functional.log_softmax(result, dim=-1), attention
 
+    def predict(
+            self,
+            dec_inp: Tensor,
+            enc_inp: Tensor,
+            enc_mask,
+            h: Tensor,
+            key=None,
+            value=None
+            ):
+        enc_lengths = self.get_lengths(enc_mask)
+        dec_lengths = torch.ones(dec_inp.shape[0])
+        if key is None and value is None:
+            enc_values, h = self.encoder(enc_inp, enc_lengths)
+        else:
+            enc_values = None
+        h, att, result, key, value = self.decoder.predict(
+            hn=h,
+            x=dec_inp,
+            lengths=dec_lengths,
+            enc_values=enc_values,
+            key=key,
+            value=value
+        )
+        return h, att, result, key, value
+
+
+class Seq2SeqBasic(nn.Module):
+    def __init__(
+            self,
+            voc_size: int,
+            emb_size: int,
+            n_layers: int,
+            hidden_size: int,
+            padding_idx: int,
+            padding_value: int,
+            p_dropout: float,
+            max_len: int,
+            *args,
+            **kwargs
+            ) -> None:
+        super().__init__()
+        self.enc_embedding = nn.Embedding(
+            voc_size,
+            emb_size,
+            padding_idx=padding_idx
+        )
+        self.dec_embedding = nn.Embedding(
+            voc_size,
+            emb_size,
+            padding_idx=padding_idx
+        )
+        self.encoder = PackedGRU(
+            input_size=emb_size,
+            hidden_size=hidden_size,
+            bidirectional=False,
+            num_layers=n_layers,
+            padding_value=padding_value
+        )
+        self.decoder = PackedGRU(
+            input_size=emb_size,
+            hidden_size=hidden_size,
+            bidirectional=False,
+            num_layers=n_layers,
+            padding_value=padding_value
+        )
+        self.attention = Attention(hidden_size)
+        self.pred_net = nn.Linear(
+            hidden_size,
+            voc_size
+        )
+        self.padding_value = padding_value
+        self.max_len = max_len
+
+    def get_lengths(self, mask: Tensor) -> Tensor:
+        return (~mask).sum(dim=-1)
+
+    def forward(
+            self,
+            enc_inp: Tensor,
+            dec_inp: Tensor,
+            enc_mask: Tensor,
+            dec_mask: Tensor
+            ):
+        enc_lengths = self.get_lengths(enc_mask).cpu()
+        dec_lengths = self.get_lengths(dec_mask).cpu()
+        enc_values, hn = self.get_enc_values(enc_inp, length=enc_lengths)
+        max_len = dec_lengths.max().item()
+        attention = list()
+        result = list()
+        for i in range(max_len):
+            inp = dec_inp[:, i:i+1]
+            preds, hn, att = self.predict(
+                enc_values=enc_values,
+                dec_inp=inp,
+                hn=hn
+                )
+            result.append(preds)
+            attention.append(att[:, -1:, :])
+        result = torch.hstack(result)
+        attention = torch.hstack(attention)
+        return nn.functional.log_softmax(result, dim=-1), attention
+
+    def predict(
+            self,
+            enc_values: Tensor,
+            dec_inp: Tensor,
+            hn: Tensor
+            ):
+        hn, att = self.attention(
+            query=hn, key=enc_values, value=enc_values
+            )
+        dec_inp = self.dec_embedding(dec_inp)
+        output, hn = self.decoder(
+            x=dec_inp, lengths=torch.ones(dec_inp.shape[0]), hn=hn
+        )
+        preds = self.pred_net(output)
+        return preds, hn, att
+
+    def get_enc_values(
+            self,
+            enc_inp: Tensor,
+            length: Union[Tensor, None] = None
+            ):
+        if length is None:
+            length = torch.ones(enc_inp.shape[0]) * enc_inp.shape[1]
+        enc_inp = self.enc_embedding(enc_inp)
+        enc_values, hn = self.encoder(enc_inp)
+        return enc_values, hn
+
 
 def get_model(
         args,
@@ -126,8 +263,12 @@ def get_model(
         voc_size: int,
         pad_idx: int
         ) -> nn.Module:
-    if args.model == 'rnn':
-        return RNN(
+    if args.model == 'seq2seqrnn':
+        return Seq2SeqRNN(
+            **get_model_args(args, voc_size, rank, pad_idx)
+        )
+    if args.model == 'basicrnn':
+        return Seq2SeqBasic(
             **get_model_args(args, voc_size, rank, pad_idx)
         )
     return Transformer(
